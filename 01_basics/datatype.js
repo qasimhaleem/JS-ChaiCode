e05550667b816813f5618ce60bd42c1a18019ac7
mrqasimhaleem <mrqasimhaleem@gmail.com>
2026-07-31 18:04:25 +0500

    datatypes and variable clear

---
+++ 01_basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all js code as newer version
+
+// alert("hello") we are using nodejs because this does not work
+
+// console.log(3 + 
+//     3 - 2
+// );
+// code readablity must be hight
+
+let name = "qasim"
+let age = 20
+let isLoggedIn = false
+
+//number 2 to power 53
+//bigInt
+//string
+//null == standalone value
+//undefined = valude not assigend
+//symbol = unique
+
+//object
+
+console.log(typeof null);
+const bigInt = 3456788765432345678456786542345678n
+
+console.log(bigInt)
+console.log(typeof bigInt)
---
+++ 01_basics/variable.js
@@ -0,0 +1,24 @@
+"use "
+
+const accountId = 10;
+let accountEmail = "mrqasimhaleem@gmail.com"
+var accountPassowrd = "mrqasimhaleem";
+accountCity = "peshawer"
+
+// accountId = 9
+
+accountEmail = "mq@gmail.com"
+accountPassowrd = "34567"
+accountCity =  "islamabad"
+
+// console.log(accountId);
+/*
+becuse the issue of block and functional scope it's bad prectace to use var
+
+
+
+*/
+
+console.table([accountId, accountEmail, accountPassowrd, accountCity])
+
+console.log(typeof(accountCity))
