29ba388f4aabbafb0d2309cda2f79e29d993335e
mrqasimhaleem <mrqasimhaleem@gmail.com>
2026-08-23 04:01:00 +0500

    Event done

---
+++ 08Events/event.js
@@ -0,0 +1,22 @@
+// // document.getElementById('cableCoils').onclick = function(e) {
+// //     console.log("hello")
+// // }
+
+// document.getElementById('cableCoils').addEventListener('click', (e) => {
+//     // console.log(e)
+//     console.log("coil clickd")
+//     e.stopPropagation()
+
+// })
+// document.getElementById('images').addEventListener('click', (e) => {
+//     // console.log(e)
+//     console.log("ul clicke")
+//     e.stopPropagation()
+// })
+
+
+// document.getElementById('').addEventListener('click', (e) => {
+//     e.preventDefault()
+
+// })
+
